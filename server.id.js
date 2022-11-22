5c6b5737db9f2b11d3ae8a556814be1e960d792f
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2022-11-22 12:50:06 -0300

    1

--- server.id.js
+++ server.id.js
@@ -1,4 +1,1 @@
-
-/*var apidata = "0450594d-a348-4217-b73c-463435c6680e";*/
-
 document.write('<script async defer data-website-id="0450594d-a348-4217-b73c-463435c6680e" src="https://devanalytics.vercel.app/umami.js"></script>');
