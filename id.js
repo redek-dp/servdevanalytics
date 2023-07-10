2c40c3b939177737d635560a3cf7924de9347822
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-07-10 13:00:02 -0300

    1

--- id.js
+++ id.js
@@ -1,1 +1,1 @@
-document.write('<script async defer data-website-id="fbb5fcf3-61a7-40f4-ae0c-c9efd16133e7" src="https://devanalytics.vercel.app/umami.js"></script>');
+document.write('<script async defer data-website-id="e29db575-15c7-4614-9420-6cef4e4275a4" src="https://devanalytics.vercel.app/umami.js"></script>');
