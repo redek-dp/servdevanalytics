d976d533af353013c67124a73df528b4eb7e608b
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-12-04 14:50:31 -0300

    v1.2

---
+++ v1/app.id.js
@@ -0,0 +1,8 @@
+var tokendd = 'aac4d334-3ace-4dd0-a382-f0c59151d2d7';
+document.write('<script async defer data-website-id="'+tokendd+'" src="https://devanalytics.vercel.app/script.js"></script>');
+
+/*
+
+id app.id
+
+*/
