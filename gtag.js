f5ecd1cc753bb78fb1c56dc7d87572a2a1f5ea0b
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2024-08-24 11:04:54 -0300

    1.1

--- gtag.js
+++ gtag.js
@@ -1,12 +1,12 @@
-var tokendd = 'G-TLFHL660XV';
+var tokenddgtag = 'G-TLFHL660XV';
 
-document.write('<script async src="https://www.googletagmanager.com/gtag/js?id='+tokendd+'"></script>');
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id='+tokenddgtag+'"></script>');
 
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', ''+tokendd+'');
+  gtag('config', ''+tokenddgtag+'');
 
 /*
 
--- v1/app.id.js
+++ v1/app.id.js
@@ -1,5 +1,13 @@
 var tokendd = '96dbdf62-ed6e-4185-b46a-ac85e0085d9e';
-document.write('<script async defer data-website-id="'+tokendd+'" src="https://devanalytics.vercel.app/script.js"></script>');
+var tokenddgtag = 'G-TLFHL660XV';
+
+document.write('<script async defer data-website-id="'+tokendd+'" src="https://devanalytics.vercel.app/script.js"></script> <script async src="https://www.googletagmanager.com/gtag/js?id='+tokenddgtag+'"></script>');
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ''+tokenddgtag+'');
 
 /*
 
