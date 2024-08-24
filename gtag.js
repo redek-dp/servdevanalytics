d1e2ea8d53989f790ef65c71da3ee8357ff317d2
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2024-08-24 10:36:53 -0300

    1.1

---
+++ gtag.js
@@ -0,0 +1,15 @@
+var tokendd = 'G-TLFHL660XV';
+
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id='+tokendd+'"></script>');
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ''+tokendd+'');
+
+/*
+
+Google tag
+
+*/
