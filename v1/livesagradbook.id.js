5751acb0cd266a166ce115bb7fceb07b621bf743
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2024-09-23 09:58:34 -0300

    1.1

--- v1/livesagradbook.id.js
+++ v1/livesagradbook.id.js
@@ -1,4 +1,4 @@
-var tokendd = '2b6388df-388a-4bd9-960e-48047e58da9e';
+var tokendd = '53cd1cac-6150-44c3-b961-e43933e848b9';
 document.write('<script async defer data-website-id="'+tokendd+'" src="https://devanalytics.vercel.app/script.js"></script>');
 
 /*
