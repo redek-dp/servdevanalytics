c0ac0b6e5b1c1ed3ef99753d5832497da62f02ba
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2026-01-30 13:04:31 -0300

    1.1

---
+++ v1/mid.id.js
@@ -0,0 +1,8 @@
+var tokendd = '59';
+document.write('<script async defer data-website-id="'+tokendd+'" src="https://devanalytics.vercel.app/script.js"></script>');
+
+/*
+
+id mid.id
+codepen.io/davidsonbpe
+*/
