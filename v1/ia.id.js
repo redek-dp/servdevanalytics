895efafce655986d8eaa40c13bacb931482dbf99
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2026-05-16 13:46:38 -0300

    1.1

---
+++ v1/ia.id.js
@@ -0,0 +1,8 @@
+var tokendd = 'cbaf0f30-b161-4c33-bfb2-d6d190a5855b';
+document.write('<script async defer data-website-id="'+tokendd+'" src="https://devanalytics.vercel.app/script.js"></script>');
+
+/*
+
+id ia.id
+iabr.pages.dev
+*/
